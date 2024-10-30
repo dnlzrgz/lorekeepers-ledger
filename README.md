a2c1e5e92cf2be78b8e08bce37c01e1fe15adc17
dnlzrgz <24715931+dnlzrgz@users.noreply.github.com>
2024-10-30 16:10:28 +0100

    refactor: remove pyproject.toml

--- README.md
+++ README.md
@@ -1,13 +1,13 @@
 # Lorekeeper's Ledger
 
-This is a small project that utilizes `Scrapy`` in a single file,`main.py``, to scrape data for testing another project I'm developing for a client. I thought it might be interesting to share the concept of using `Scrapy` in a single file, so I decided to publish it.
-
-To run the project you just need to clone this repository and run:
+This is a small project that utilizes `Scrapy`` in a single file,`main.py`, to scrape data for testing another project I'm developing for a client. I thought it might be interesting to share the concept of using`Scrapy`in a single file, so I decided to publish it.
+Another peculiarity of this project is that it uses`uv`to run the script without relying on a`pyproject.toml` to handle the dependencies.
+To run the project, you just need to clone this repository and run:
 
 ```bash
 cd lorekeepers-ledger
 
 # &
 
-python main.py
+uv run main.py
 ```
--- main.py
+++ main.py
@@ -1,54 +1,51 @@
+# /// script
+# requires-python = ">=3.12"
+# dependencies = [
+#   "environs>=11.0.0",
+#   "markdown>=3.7",
+#   "scrapy-playwright>=0.0.41",
+#   "scrapy>=2.11.2",
+# ]
+# ///
+
 import scrapy
 from environs import Env
 from itemloaders.processors import MapCompose, TakeFirst
 from scrapy.crawler import CrawlerProcess
 from scrapy.loader import ItemLoader
 from w3lib.html import remove_tags
+from scrapy_playwright.page import PageMethod
 
 
 def remove_white_spaces(value):
     return value.strip().replace("\n", "").replace("\r", "")
 
 
-def remove_currency_symbol(value):
-    return float(value.replace("€", "").strip().replace(",", "."))
-
-
 def remove_dashes(value):
     return value.replace("-", "")
 
 
-def format_author_name(value):
-    if "/" in value:
-        value = value.split("/")[0].strip()
-
-    if "," in value:
-        last, first = value.split(",", 1)
-        return f"{first.strip()} {last.strip()}"
-
-    return value
-
-
-class Book(scrapy.Item):
+class Product(scrapy.Item):
     title = scrapy.Field(
         input_processor=MapCompose(remove_tags, remove_white_spaces),
         output_processor=TakeFirst(),
     )
     author = scrapy.Field(
-        input_processor=MapCompose(
-            remove_tags, remove_white_spaces, format_author_name
-        ),
+        input_processor=MapCompose(remove_tags, remove_white_spaces),
         output_processor=TakeFirst(),
     )
     url = scrapy.Field()
+    parent_url = scrapy.Field()
+    image = scrapy.Field()
     price = scrapy.Field(
         input_processor=MapCompose(
-            remove_tags, remove_white_spaces, remove_currency_symbol
+            remove_tags,
+            remove_white_spaces,
         ),
         output_processor=TakeFirst(),
     )
     isbn = scrapy.Field(
-        input_processor=MapCompose(remove_tags, remove_white_spaces, remove_dashes),
+        input_processor=MapCompose(remove_tags, remove_white_spaces),
         output_processor=TakeFirst(),
     )
     publisher = scrapy.Field(
@@ -59,38 +56,110 @@ class Book(scrapy.Item):
         input_processor=MapCompose(remove_tags, remove_white_spaces),
         output_processor=TakeFirst(),
     )
+    summary = scrapy.Field()
 
 
-class TodosTusLibrosSpider(scrapy.Spider):
-    name = "todos_tus_libros"
-    start_urls = ["https://www.todostuslibros.com/mas_vendidos"]
+class CasaDelLibroSpider(scrapy.Spider):
+    name = "casa_del_libro"
+    start_urls = [
+        "https://www.casadellibro.com/libros/comics-y-manga-infantil-y-juvenil/manga-juvenil/shonen/412004002",
+        "https://www.casadellibro.com/libros/comics/manga/seinen/411006004",
+        "https://www.casadellibro.com/libros/comics-y-manga-infantil-y-juvenil/manga-juvenil/shojo/412004001",
+        "https://www.casadellibro.com/libros/arte/101000000",
+        "https://www.casadellibro.com/libros/comics/411000000",
+        "https://www.casadellibro.com/libros/literatura-en-otros-idiomas/124000000",
+    ]
+    meta = {
+        "playwright": True,
+        "playwright_include_page": True,
+        "playwright_page_goto_kwargs": {
+            "wait_until": "networkidle",
+        },
+        "playwright_page_methods": [
+            PageMethod("evaluate", "window.scrollBy(0, document.body.scrollHeight)"),
+        ],
+    }
 
-    def parse(self, response):
-        books = response.css("li.book")
-        for book in books:
-            book_url = book.css("h2.title a::attr(href)").get()
-            if book_url:
-                yield response.follow(book_url, self.parse_book)
+    def start_requests(self):
+        for url in self.start_urls:
+            yield scrapy.Request(url, self.parse, meta={**self.meta, "base_url": url})
 
-        next_page = response.css(
-            "ul.pagination li.page-item a[rel='next']::attr(href)"
-        ).get()
-        if next_page:
-            yield response.follow(next_page, self.parse)
+    async def parse(self, response):
+        page = response.meta["playwright_page"]
+        await page.wait_for_selector("div.products")
 
-    def parse_book(self, response):
-        loader = ItemLoader(Book(), response=response)
-        loader.add_css("title", "h1.title::text")
-        loader.add_css("author", "a.author::text")
-        loader.add_value("url", response.url)
-        loader.add_css("price", "div.total-book-price strong::text")
-        loader.add_css("isbn", "dt:contains('ISBN:') + dd::text")
-        loader.add_css("publisher", "dt:contains('Editorial:') + dd a::text")
-        loader.add_css(
-            "publication_date", "dt:contains('Fecha publicación :') + dd::text"
+        product_links = await page.query_selector_all("div.compact-product a")
+        for link in product_links:
+            href = await link.get_attribute("href")
+            yield response.follow(
+                href,
+                self.parse_product,
+                meta={
+                    **self.meta,
+                    "parent_url": response.url,
+                },
+            )
+
+        last_page_button = await page.query_selector(
+            "div.paginator button.btn:last-child"
         )
+        if last_page_button:
+            base_url = response.meta["base_url"]
+            last_page_text = await last_page_button.inner_text()
+            last_page_number = int(last_page_text.strip())
+            for page_number in range(2, last_page_number + 1):
+                next_page_url = f"{base_url}/p{page_number}"
+                yield scrapy.Request(
+                    next_page_url, self.parse, meta={**self.meta, "base_url": base_url}
+                )
+
+        await page.close()
+
+    async def parse_product(self, response):
+        page = response.meta["playwright_page"]
+
+        loader = ItemLoader(Product(), response=response)
+        loader.add_value("url", response.url)
+        loader.add_value("parent_url", response.meta["parent_url"])
+
+        title = await page.query_selector(".titleProducto")
+        if title:
+            loader.add_value("title", await title.inner_text())
+
+        author = await page.query_selector(".autor h4")
+        if author:
+            loader.add_value("author", await author.inner_text())
+
+        price = await page.query_selector(".info-price p")
+        if price:
+            loader.add_value("price", await price.inner_text())
+
+        publisher = await page.query_selector(
+            ".campo[data-campo='Editorial'] span.truncate-text"
+        )
+        if publisher:
+            loader.add_value("publisher", await publisher.inner_text())
+
+        isbn = await page.query_selector(".campo[data-campo='ISBN'] span.truncate-text")
+        if isbn:
+            loader.add_value("isbn", await isbn.inner_text())
+
+        publication_date = await page.query_selector(
+            ".campo[data-campo='Fecha de lanzamiento'] span.truncate-text"
+        )
+        if publication_date:
+            loader.add_value("publication_date", await publication_date.inner_text())
+
+        summary = await page.query_selector(".resumen-content")
+        if summary:
+            loader.add_value("summary", await summary.inner_text())
+
+        image = await page.query_selector("div.portada img")
+        if image:
+            loader.add_value("image", await image.get_attribute("src"))
 
         yield loader.load_item()
+        await page.close()
 
 
 if __name__ == "__main__":
@@ -100,7 +169,7 @@ if __name__ == "__main__":
     process = CrawlerProcess(
         settings={
             "FEEDS": {
-                "books.csv": {"format": "csv"},
+                "products.csv": {"format": "csv"},
             },
             "USER_AGENT": env(
                 "USER_AGENT",
@@ -150,12 +219,20 @@ if __name__ == "__main__":
                 "ROBOTSTXT_OBEY",
                 False,
             ),
+            "DOWNLOAD_HANDLERS": {
+                "http": "scrapy_playwright.handler.ScrapyPlaywrightDownloadHandler",
+                "https": "scrapy_playwright.handler.ScrapyPlaywrightDownloadHandler",
+            },
             "TWISTED_REACTOR": env(
                 "TWISTED_REACTOR",
                 "twisted.internet.asyncioreactor.AsyncioSelectorReactor",
             ),
+            "PLAYWRIGHT_BROWSER_TYPE": "chromium",
+            "PLAYWRIGHT_LAUNCH_OPTIONS": {
+                "headless": env.bool("PLAYWRIGHT_LAUNCH_OPTIONS_HEADLESS", False),
+            },
         }
     )
 
-    process.crawl(TodosTusLibrosSpider)
+    process.crawl(CasaDelLibroSpider)
     process.start()
